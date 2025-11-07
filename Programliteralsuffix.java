1d816b7c86860e128439eeb9279599875eeab9c8
Sangeetha <tooalonetobesane@gmail.com>
2025-11-07 16:06:42 +0530

    Add files via upload

---
+++ Programliteralsuffix.java
@@ -0,0 +1,24 @@
+package charTypecasting;
+
+public class Programliteralsuffix {
+
+	public static void main(String[] args) {
+		long a = 65;
+		long a1 = 65l;
+		long b = 3873476;
+		long b1 = 8767543l;
+		long c = 7493859398787L; // for long suffix l/L is necessary
+		long d = 749385l;
+		
+		
+		System.out.println(a);
+		System.out.println(a1);
+		System.out.println(b);
+		System.out.println(b1);
+		System.out.println(c);
+		System.out.println(d);
+		
+		
+	}
+
+}
---
+++ TypecatingBool.java
@@ -0,0 +1,78 @@
+package charTypecasting;
+
+public class TypecatingBool {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 boolean ch = true;
+		    System.out.println(ch);
+		    int num = ch ;
+		    System.out.println(num);
+		  
+		    boolean ch1 = true;
+		    System.out.println(ch1);
+		    byte num1 = (byte)ch1 ;
+		    System.out.println(num1);
+		  
+		    
+		    boolean ch = true;
+		    System.out.println(ch2);
+		    short num2 = (byte) ch2 ;
+		   System.out.println(num2);
+		  
+		    
+		   boolean ch = true;
+		    System.out.println(ch3);
+		    long num3 = ch3 ;
+		    System.out.println(num3);
+		  
+		    
+		    boolean ch = true;
+		    System.out.println(ch4);
+		    float num4 = ch4 ;
+		    System.out.println(num4);
+		  
+		    
+		    boolean ch = true;
+		    System.out.println(ch5);
+		    double num5 = ch5 ;
+		    System.out.println(num5);
+		  
+		    
+		 //ExplicitTypecating
+		    
+		    int word = 80;
+		    System.out.println(word);
+		    boolean letter0 = (boolean) word ;
+		    System.out.println(letter0);
+		    
+		    byte word1 = 81;
+		    System.out.println(word1);
+		    boolean letter1 = (boolean) word1 ;
+		    System.out.println(letter1);
+		    
+		    short word2 = 82;
+		    System.out.println(word2);
+		    boolean letter2 = (boolean) word2 ;
+		    System.out.println(letter2);
+		    
+		    long word3 = 83;
+		    System.out.println(word3);
+		    boolean letter3 = (boolean) word3 ;
+		    System.out.println(letter3);
+		    
+		    float word4 = 85.85f;
+		    System.out.println(word4);
+		    boolean letter4 = (boolean) word4 ;
+		    System.out.println(letter4);
+		    
+		    double word5 = 84.90;
+		    System.out.println(word5);
+		    boolean letter5 = (boolean) word5 ;
+		    System.out.println(letter5);
+		    
+		    
+		    
+	}
+
+}
